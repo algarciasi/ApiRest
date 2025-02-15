40ee911427a0b86a06642bfcac59bb23b33654a6
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-02-15 08:16:27 +0100

    Creacion de routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { UsuariosListComponent } from './pages/usuarios-list/usuarios-list.component';
+import { UsuariosFormComponent } from './pages/usuarios-form/usuarios-form.component';
+import { UsuarioCardComponent } from './components/usuario-card/usuario-card.component';
+import { UsuariosService } from './services/usuarios.service';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", pathMatch: "full", redirectTo: "usuarios"},
+    {path:"home", component: UsuariosListComponent},
+    {path:"newUser", component: UsuariosFormComponent},
+    {path:"updateUser", component: UsuariosService},
+    {path:"user", component: UsuarioCardComponent},
+    {path:"**", redirectTo: "Error 404"},
+
+];
