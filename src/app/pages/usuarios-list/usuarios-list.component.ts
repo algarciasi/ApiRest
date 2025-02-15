6f3494e8612e065e9d0e870a9a928351b9872aa2
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-02-15 20:41:06 +0100

    edicion con paginacion basica

--- src/app/pages/usuarios-list/usuarios-list.component.ts
+++ src/app/pages/usuarios-list/usuarios-list.component.ts
@@ -2,11 +2,12 @@ import { UsuariosService } from './../../services/usuarios.service';
 import { Component, inject } from '@angular/core';
 import { Usuario } from '../../interfaces/usuario';
 import { UsuarioCardComponent } from "../../components/usuario-card/usuario-card.component";
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-usuarios-list',
   standalone: true,
-  imports: [UsuarioCardComponent],
+  imports: [UsuarioCardComponent, NgxPaginationModule],
   templateUrl: './usuarios-list.component.html',
   styleUrl: './usuarios-list.component.css'
 })
@@ -15,6 +16,10 @@ export class UsuariosListComponent {
   arrUsuarios: Usuario[];
   usuarioService = inject(UsuariosService);
 
+  usuariosPaginados: Usuario[] = [];
+  paginaActual: number = 1;
+  usuariosPorPagina: number = 6;
+
   constructor(){
     this.arrUsuarios =[];
   }
@@ -22,7 +27,15 @@ export class UsuariosListComponent {
   ngOnInit(): void{
     this.usuarioService.getAllWithObservables().subscribe((data: any) => {
       this.arrUsuarios = data.results;
+      this.cambiarPagina(1);
     });
+  }
+
+  cambiarPagina(pagina: number): void {
+    this.paginaActual = pagina;
+    let inicio = (pagina - 1) * this.usuariosPorPagina;
+    let fin = inicio + this.usuariosPorPagina;
+    this.usuariosPaginados = this.arrUsuarios.slice(inicio, fin);
   }
 
 }
