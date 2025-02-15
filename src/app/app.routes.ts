50204f0f8c8a40d1c780d9cb442ca25df5a870d6
Alberto Garcia <albertomiguelgarciasimo@outlook.com>
2025-02-15 14:14:47 +0100

    parent hecho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { UsuariosViewComponent } from './pages/usuarios-view/usuarios-view.component';
 import { Routes } from '@angular/router';
 import { UsuariosListComponent } from './pages/usuarios-list/usuarios-list.component';
 import { UsuariosFormComponent } from './pages/usuarios-form/usuarios-form.component';
@@ -8,8 +9,8 @@ export const routes: Routes = [
     {path:"", pathMatch: "full", redirectTo: "usuarios"},
     {path:"home", component: UsuariosListComponent},
     {path:"newUser", component: UsuariosFormComponent},
-    {path:"updateUser", component: UsuariosService},
-    {path:"user", component: UsuarioCardComponent},
+    {path:"update/user/:_id", component: UsuariosFormComponent},
+    {path:"user/:_id", component: UsuariosViewComponent},
     {path:"**", redirectTo: "Error 404"},
 
 ];
--- src/app/pages/usuarios-view/usuarios-view.component.ts
+++ src/app/pages/usuarios-view/usuarios-view.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsuariosService } from '../../services/usuarios.service';
+import { ActivatedRoute } from '@angular/router';
+import { Usuario } from '../../interfaces/usuario';
+import { BotoneraComponent } from "../../components/botonera/botonera.component";
 
 @Component({
   selector: 'app-usuarios-view',
   standalone: true,
-  imports: [],
+  imports: [BotoneraComponent],
   templateUrl: './usuarios-view.component.html',
   styleUrl: './usuarios-view.component.css'
 })
 export class UsuariosViewComponent {
+
+  usuariosService = inject(UsuariosService);
+  activatedRoute = inject(ActivatedRoute);
+
+  miUsuario !: Usuario;
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: any) => {
+      let _id: string = params._id as string;
+  
+      this.usuariosService.getByIdWithObservable(_id).subscribe({
+        next: (serie) => {
+          this.miUsuario = serie;
+        },
+        error: (err) => {
+          console.error('Error al llamar a la API:', err);
+        }
+      });
+    });
+  }
+  
 
 }
